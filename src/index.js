357440fb62dbae971b3768b0adf7d0796e46e0e2
Rózsa Zoltán <67325669+rozsazoltan@users.noreply.github.com>
2026-02-01 11:47:50 +0100

    feat: migrate from CJS to ESM

--- src/index.js
+++ src/index.js
@@ -1,57 +1,58 @@
-const plugin = require("tailwindcss/plugin");
+import plugin from 'tailwindcss/plugin';
+import heroPatterns from './patterns.js';
 
-const heroPatterns = require("./patterns");
+export default plugin(
+  ({ addUtilities, theme }) => {
+    const allowedShades = theme('heroPatternsShades', []);
+    const allowedColors = theme('heroPatternsColors', []);
+    const allowedOpacities = theme('heroPatternsOpacities', ['10', '50', '90']);
 
-module.exports = plugin(
-  function ({ addUtilities, theme }) {
-    const allowedShades = theme('heroPatternsShades', [])
-    const allowedColors = theme('heroPatternsColors', [])
-    const allowedOpacities = theme('heroPatternsOpacities', ['10', '50', '90'])
-
-    const patterns = theme('heroPatterns', {})
-    const colors = theme('colors', {})
+    const patterns = theme('heroPatterns', {});
+    const colors = theme('colors', {});
     const opacity = Object.fromEntries(
       Object.entries(theme('opacity', {})).filter(
         ([key]) => !allowedOpacities.length || allowedOpacities.includes(key)
       )
-    )
+    );
 
-    // flatten colors
+    // Flatten colors
     const flattenColors = {};
-    Object.entries(colors).map(([name, color]) => {
-      if (typeof color === "object") {
-        Object.entries(color).map(([nestedName, nestedColor]) => {
-          if (allowedShades.length && !allowedShades.includes(nestedName)) {
-            return;
-          }
-          if (allowedColors.length && !allowedColors.includes(name)) {
-            return;
-          }
-          flattenColors[
-            `${name}${nestedName === "default" ? "" : `-${nestedName}`}`
-          ] = nestedColor;
-        });
+    for (const [name, color] of Object.entries(colors)) {
+      if (typeof color === 'object') {
+        for (const [shade, value] of Object.entries(color)) {
+          if (allowedShades.length && !allowedShades.includes(shade)) continue;
+          if (allowedColors.length && !allowedColors.includes(name)) continue;
+
+          const suffix = shade === 'default' ? '' : `-${shade}`;
+          flattenColors[`${name}${suffix}`] = value;
+        }
       } else {
         flattenColors[name] = color;
       }
-    });
+    }
 
     const newUtilities = {};
-    Object.entries(patterns).map(([name, pattern]) => {
-      Object.entries(flattenColors).map(([colorName, color]) => {
-        const coloredPattern = pattern.replace('{{color}}', color.toString().replace('#', '%23'))
+    for (const [patternName, pattern] of Object.entries(patterns)) {
+      for (const [colorName, color] of Object.entries(flattenColors)) {
+        const encodedColor = color.toString().replace('#', '%23');
+        const coloredPattern = pattern.replace('{{color}}', encodedColor);
 
-        newUtilities[`.heropattern-${name}-${colorName}`] = {
-          backgroundImage: coloredPattern
-            .replace("{{opacity}}", 1),
+        newUtilities[`.heropattern-${patternName}-${colorName}`] = {
+          backgroundImage: coloredPattern.replace('{{opacity}}', 1),
         };
-        Object.entries(opacity).map(([opacityName, opacityValue]) => {
-          newUtilities[`.heropattern-${name}-${colorName}\\/${opacityName}`] = {
-            backgroundImage: coloredPattern.replace("{{opacity}}", opacityValue),
+
+        for (const [opacityName, opacityValue] of Object.entries(opacity)) {
+          newUtilities[
+            `.heropattern-${patternName}-${colorName}\\/${opacityName}`
+          ] = {
+            backgroundImage: coloredPattern.replace(
+              '{{opacity}}',
+              opacityValue
+            ),
           };
-        });
-      });
-    });
+        }
+      }
+    }
 
     addUtilities(newUtilities);
   },
