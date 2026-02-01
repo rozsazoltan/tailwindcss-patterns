ddd2535009433af118425005a13e2000fdc45b69
Rózsa Zoltán <67325669+rozsazoltan@users.noreply.github.com>
2026-02-01 12:38:30 +0100

    chore!: convert addUtilities to matchUtilities

--- src/index.js
+++ src/index.js
@@ -2,59 +2,31 @@ import plugin from 'tailwindcss/plugin';
 import patterns from './patterns.js';
 
 export default plugin(
-  ({ addUtilities, theme }) => {
-    const allowedShades = theme('patternShade', []);
-    const allowedColors = theme('patternColor', []);
-    const allowedOpacities = theme('patternOpacity', ['10', '50', '90']);
-
+  ({ matchUtilities, theme }) => {
     const patterns = theme('pattern', {});
-    const colors = theme('colors', {});
-    const opacity = Object.fromEntries(
-      Object.entries(theme('opacity', {})).filter(
-        ([key]) => !allowedOpacities.length || allowedOpacities.includes(key)
-      )
-    );
 
-    // Flatten colors
-    const flattenColors = {};
-    for (const [name, color] of Object.entries(colors)) {
-      if (typeof color === 'object') {
-        for (const [shade, value] of Object.entries(color)) {
-          if (allowedShades.length && !allowedShades.includes(shade)) continue;
-          if (allowedColors.length && !allowedColors.includes(name)) continue;
+    for (const [patternName, patternSvg] of Object.entries(patterns)) {
+      matchUtilities(
+        {
+          [`pattern-${patternName}`]: (value, { modifier }) => {
+            const opacityValue = modifier !== null ? modifier : '1';
+            const encodedColor = value.toString().replace('#', '%23');
 
-          const suffix = shade === 'default' ? '' : `-${shade}`;
-          flattenColors[`${name}${suffix}`] = value;
+            let coloredPattern = patternSvg.replace('{{color}}', encodedColor);
+            coloredPattern = coloredPattern.replace('{{opacity}}', opacityValue);
+
+            return {
+              backgroundImage: coloredPattern,
+            };
+          },
+        },
+        {
+          values: theme('colors'),
+          modifiers: theme('opacity'), 
+          type: ['color', 'any'], 
         }
-      } else {
-        flattenColors[name] = color;
-      }
+      );
     }
-
-    const newUtilities = {};
-    for (const [patternName, pattern] of Object.entries(patterns)) {
-      for (const [colorName, color] of Object.entries(flattenColors)) {
-        const encodedColor = color.toString().replace('#', '%23');
-        const coloredPattern = pattern.replace('{{color}}', encodedColor);
-
-        newUtilities[`.pattern-${patternName}-${colorName}`] = {
-          backgroundImage: coloredPattern.replace('{{opacity}}', 1),
-        };
-
-        for (const [opacityName, opacityValue] of Object.entries(opacity)) {
-          newUtilities[
-            `.pattern-${patternName}-${colorName}\\/${opacityName}`
-          ] = {
-            backgroundImage: coloredPattern.replace(
-              '{{opacity}}',
-              opacityValue
-            ),
-          };
-        }
-      }
-    }
-
-    addUtilities(newUtilities);
   },
   {
     theme: {
